da0c82287170281c7492bc669b944947b9c53b84
Daniel <d.popov0470@gmail.com>
2025-07-18 01:46:49 +0300

    feat: add logic for playing a single round

--- app.js
+++ app.js
@@ -3,10 +3,53 @@ let humanScore = 0;
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
-    return randomNumber === 0 ? "Rock" : randomNumber === 1 ? "Paper" : "Scissors";
+    return randomNumber === 0
+        ? "rock"
+        : randomNumber === 1
+            ? "paper"
+            : "scissors";
 }
 
 function getHumanChoice() {
     let humanChoice = prompt("Rock, Paper, Scissors?", "");
     return humanChoice;
 }
+
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        console.log("It's a tie.");
+    }
+
+    if (
+        (computerChoice === "rock" && humanChoice === "scissors") ||
+        (computerChoice === "paper" && humanChoice === "rock") ||
+        (computerChoice === "scissors" && humanChoice === "paper")
+    ) {
+        computerScore++;
+        console.log(
+            `You lose! ${computerChoice.replace(computerChoice[0],
+                computerChoice[0].toUpperCase()
+            )} beats ${humanChoice.replace(humanChoice[0],
+                humanChoice[0].toUpperCase()
+            )}.`
+        );
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        console.log(
+            `You win! ${humanChoice.replace(humanChoice[0],
+                humanChoice[0].toUpperCase()
+            )} beats ${computerChoice.replace(computerChoice[0],
+                computerChoice[0].toUpperCase()
+            )}.`
+        );
+    }
+}
+
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice().toLowerCase();
+
+playRound(computerChoice, humanChoice);
